0758fcbb5d3d44f17f29207a033028d4bc4a8093
Darustet <58421506+Darustet@users.noreply.github.com>
2024-02-01 14:47:54 +0200

    Added DBConnect.js, connection throws error
    Added DBConnect.js to connect to database. Currently connection is not working.

---
+++ Database/DBConnect.js
@@ -0,0 +1,26 @@
+var mysql = require("mysql");
+
+var hostname = "ep9.h.filess.io";
+var database = "otp_lowersitof";
+var port = "3305";
+var username = "otp_lowersitof";
+var password = "a0801bab4112095dc98fc267777e15f507c4560b";
+
+
+var con = mysql.createConnection({
+    host: hostname,
+    user: username,
+    password,
+    database,
+    port,
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+con.query("SELECT 1+1").on("result", function (row) {
+    console.log("in con.query");
+    console.log(row);
+});
