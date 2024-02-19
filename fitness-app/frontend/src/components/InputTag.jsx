4816c6b4dc51af3d53dc05dfece6c31d21858301
Darustet <58421506+Darustet@users.noreply.github.com>
2024-02-19 14:38:32 +0200

    Added 'Create Event' page. 'Add tags' function in progress
    Added 'Create Event' page. First draft of 'add tags' function.

---
+++ fitness-app/frontend/src/components/InputTag.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { WithContext as ReactTags } from "react-tag-input";
+
+// Specifies which characters should terminate tags input. An array of character codes.
+const KeyCodes = {
+    comma: 188,
+    enter: 13,
+};
+
+const delimiters = [KeyCodes.comma, KeyCodes.enter];
+
+const InputTag = () => {
+    const [tags, setTags] = React.useState([]);
+
+    // Method to delete tag from Array
+    const handleDelete = (i) => {
+        setTags(tags.filter((tag, index) => index !== i));
+    };
+
+    // Method to Add tag into Array
+    const handleAddition = (tag) => {
+        setTags([...tags, tag]);
+    };
+    return (
+        <div id="tags">
+            <ReactTags
+                tags={tags}
+                delimiters={delimiters}
+                handleDelete={handleDelete}
+                handleAddition={handleAddition}
+                inputFieldPosition="bottom"
+                autocomplete
+                allowDragDrop={false}
+            />
+        </div>
+    );
+};
+
+export default InputTag;
