4f5bef13186791412043fc054d671619f8799794
Sami Abo Ramadan <Sami.AboRamadan@metropolia.fi>
2024-02-21 15:39:44 +0200

    update

---
+++ fitness-app/frontend/src/pages/Login/login.jsx
@@ -0,0 +1,32 @@
+<<<<<<< Updated upstream
+=======
+import style from "./Login.module.scss";
+
+const Login = () => {
+	return (
+		<div className={style["login-container "]}>
+			<h2>Login</h2>
+			<div className={style["form-group"]}>
+				<label htmlFor={"username"}></label>
+				<input type="text" id="username" name="username" placeholder="Enter your email adress or username" />
+			</div>
+
+			<div className={style["form-group"]}>
+				<label htmlFor="password">Password:</label>
+				<input type="password" id="password" name="password" placeholder="Enter your password" />
+			</div>
+
+			<div className={style["form-group"]}>
+				<button type="button">Login</button>
+			</div>
+
+			<div className="options">
+				<a href="#">Register</a>
+				<a href="#">Forgot password</a>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
+>>>>>>> Stashed changes
