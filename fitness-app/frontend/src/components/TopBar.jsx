ee670e0af1df3d637d39fdd54f64e8d833fca2de
PatrickJaloScott <patrickjalo.scott@gmail.com>
2024-02-23 00:25:45 +0200

    * switches import variable to string literal in image src reference

--- fitness-app/frontend/src/components/TopBar.jsx
+++ fitness-app/frontend/src/components/TopBar.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import profileIcon from './blank-avatar-icon.jpg'
 
 const TopBar = () => {
     const avatarSize = 64;
     return(
         <div className="top-bar">
             <img alt="profile picture"
-                 src={profileIcon}
+                 src={'./blank-avatar-icon.jpg'}
                  width={avatarSize}
                  height={avatarSize}/>
         </div>
