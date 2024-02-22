14f4a63d9f22f96750bf873534d40f450cae94e7
adeahmed <abdi-samad.ahmed@outlook.com>
2024-02-22 15:26:23 +0200

    päivitys

--- fitness-app/frontend/src/pages/Home.jsx
+++ fitness-app/frontend/src/pages/Home.jsx
@@ -1,26 +1,21 @@
-import Calendar from '../components/Calendar';
-import {useState} from 'react';
-import Posts from '../components/Posts';
+import Calendar from "../components/Calendar/Calendar";
+import { useState } from "react";
+import Posts from "../components/Posts";
 
 const Home = () => {
-    const placeholderPosts = [
-        "Post",
-        "Post",
-        "Post",
-        "Post"
-    ];
-    return (
-        <>
-            <h1>Home</h1>
-            <main>
-                <Posts postData={placeholderPosts}/>
-                <form>
-                    <input type="button" value="New event (not implemented)"/>
-                </form>
-            </main>
-            <Calendar />
-        </>
-    );
-}
+	const placeholderPosts = ["Post", "Post", "Post", "Post"];
+	return (
+		<>
+			<h1>Home</h1>
+			<main>
+				<Posts postData={placeholderPosts} />
+				<form>
+					<input type="button" value="New event (not implemented)" />
+				</form>
+			</main>
+			<Calendar />
+		</>
+	);
+};
 
 export default Home;
