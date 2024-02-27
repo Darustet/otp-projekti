111cda753bfe957071ee86be0d3920f1361c89d7
Darustet <58421506+Darustet@users.noreply.github.com>
2024-02-27 15:00:18 +0200

    Revert "moi"
    This reverts commit ced235137cc30b23101de7d102b70aa9fe39d0d8.

--- fitness-app/frontend/src/components/InputTag.jsx
+++ fitness-app/frontend/src/components/InputTag.jsx
@@ -1,40 +1,39 @@
-/*
-// import React from "react";
-// import { WithContext as ReactTags } from "react-tag-input";
+import React from "react";
+import { WithContext as ReactTags } from "react-tag-input";
 
-// // Specifies which characters should terminate tags input. An array of character codes.
-// const KeyCodes = {
-//     comma: 188,
-//     enter: 13,
-// };
+// Specifies which characters should terminate tags input. An array of character codes.
+const KeyCodes = {
+	comma: 188,
+	enter: 13,
+};
 
-// const delimiters = [KeyCodes.comma, KeyCodes.enter];
+const delimiters = [KeyCodes.comma, KeyCodes.enter];
 
-// const InputTag = () => {
-//     const [tags, setTags] = React.useState([]);
+const InputTag = () => {
+	const [tags, setTags] = React.useState([]);
 
-//     // Method to delete tag from Array
-//     const handleDelete = (i) => {
-//         setTags(tags.filter((tag, index) => index !== i));
-//     };
+	// Method to delete tag from Array
+	const handleDelete = (i) => {
+		setTags(tags.filter((tag, index) => index !== i));
+	};
 
-//     // Method to Add tag into Array
-//     const handleAddition = (tag) => {
-//         setTags([...tags, tag]);
-//     };
-//     return (
-//         <div id="tags">
-//             <ReactTags
-//                 tags={tags}
-//                 delimiters={delimiters}
-//                 handleDelete={handleDelete}
-//                 handleAddition={handleAddition}
-//                 inputFieldPosition="bottom"
-//                 autocomplete
-//                 allowDragDrop={false}
-//             />
-//         </div>
-//     );
-// };
+	// Method to Add tag into Array
+	const handleAddition = (tag) => {
+		setTags([...tags, tag]);
+	};
+	return (
+		<div id="tags">
+			<ReactTags
+				tags={tags}
+				delimiters={delimiters}
+				handleDelete={handleDelete}
+				handleAddition={handleAddition}
+				inputFieldPosition="bottom"
+				autocomplete
+				allowDragDrop={false}
+			/>
+		</div>
+	);
+};
 
-// export default InputTag;*/
+export default InputTag;
