fef725c11023b0ba5cc2982b728676d66cb3f0f4
PatrickJaloScott <patrickjalo.scott@gmail.com>
2024-02-29 15:22:59 +0200

    * NavBar.jsx: simplified to use props * Home.jsx: changed component to type function * App.jsx: reorganized imports and added route array, along with routing function component and implementation

--- fitness-app/frontend/src/App.jsx
+++ fitness-app/frontend/src/App.jsx
@@ -1,7 +1,5 @@
-
-
+import "./App.scss";
 import NavBar from "./components/NavBar/NavBar.jsx";
-//import DomainRouting from './components/DomainRouting';
 import TopBar from './components/TopBar';
 import Home from "./pages/Home";
 import Explore from './pages/Explore';
@@ -11,29 +9,62 @@ import Profile from "./pages/Profile/Profile.jsx";
 import Register from "./pages/Register/Register.jsx";
 import ForgotPassword from "./pages/ForgotPassword/Forgot_password.jsx";
 import { NotificationProvider } from "./NotificationsData/Notification";
-
-import "./App.scss";
-
-const { BrowserRouter, Routes, Route } = require("react-router-dom");
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
+	const routeData = [
+		{
+			name: "Home",
+			url: "/home",
+			component: <Home/>
+		},
+		{
+			name: "Explore",
+			url: "/explore",
+			component: <Explore />
+		},
+		{
+			name: "Create Event",
+			url: "/create-event",
+			component: <CreateEvent />
+		},
+		{
+			name: "Login",
+			url: "/login",
+			component: <Login />
+		},
+		{
+			name: "Profile",
+			url: "/profile",
+			component: <Profile />
+		},
+		{
+			name: "Register",
+			url: "/register",
+			component: <Register />
+		},
+		{
+			url: "/forgot-password",
+			component: <ForgotPassword />
+		}
+	];
+
+	function routing(data) {
+		return data.map((item, index) =>
+			<Route key={index}
+			exact path={item.url}
+			element={item.component}/>
+		)
+	}
+
 	return (
 		<div className="app">
 			<BrowserRouter>
 				<NotificationProvider>
 					<TopBar />
-					<NavBar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/explore" element={<Explore />} />
-						<Route path="/create-event" element={<CreateEvent />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/profile" element={<Profile />} />
-						<Route path="/register" element={<Register />} />
-						<Route path="/forgot-password" element={<ForgotPassword />} />
-						
-					</Routes>
+					<NavBar linkData={routeData} />
+					<Routes>{routing(routeData)}</Routes>
 				</NotificationProvider>
 	
 			</BrowserRouter>	
--- fitness-app/frontend/src/components/NavBar/NavBar.jsx
+++ fitness-app/frontend/src/components/NavBar/NavBar.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import style from "./NavBar.module.scss";
 import { Link } from "react-router-dom";
 
-export default function NavBar() {
+export default function NavBar({linkData}) {
+	const filteredData = linkData.filter(item => item.name);
 	return (
 		<nav className={style["Nav-bar"]}>
 			<img src="" alt="logo" />
-			<Link to="/">home</Link>
-			<Link to="/profile">profile</Link>
-			<Link to="/explore">explore</Link>
-			<Link to="/create-event">create event</Link>
-			<Link to="/login">login</Link>
-			<Link to="/register">register</Link>
-			<Link to="/forgot-password">forgot password</Link>
+			{filteredData.map((item, index) =>
+				<Link key={index} to={item.url}>{item.name}</Link>
+			)}
 		</nav>
 	);
 }
