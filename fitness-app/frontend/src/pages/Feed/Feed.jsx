801031059a67fb1eceece8d270c5da1b3165fda4
Darustet <58421506+Darustet@users.noreply.github.com>
2024-03-03 22:32:50 +0200

    Postausta tyylitelty, lisätty NotificationCard componentteihin

--- fitness-app/frontend/src/pages/Feed/Feed.jsx
+++ fitness-app/frontend/src/pages/Feed/Feed.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import Calendar from "../../components/Calendar/Calendar";
+import NotificationCard from "../../components/NotificationCard/NotificationCard";
 
+const NotificationFeed = () => {
+
+  return (
+    <div>
+      <div>
+        {/* Example card, idea to change to a feed consisting of similar cards */}
+        <NotificationCard/>
+        <Calendar/>
+      </div>
+    </div>
+  )
+}
+
+/*
 const NotificationCard = () => {
   return (
     <>
     <div style={styles.wrapper}>
       <div style={styles.card}>
         <div style={styles.header}>
-          <span style={styles.headerText}>Silver · Today @ 9pm</span>
+          <span style={styles.title}>Neighbourhood basketball</span>
           <img style={styles.profileImage} src="path_to_image.jpg" alt="Profile" />
         </div>
+        <div style={styles.timeAndLocation}>
+          <span style={styles.headerText}>Silver · Today @ 9pm</span>
+        </div>
         <div style={styles.content}>
-          <h2 style={styles.title}>watch party, let's go</h2>
+          <text tyle={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</text>
+        </div>
+        <div styles={styles.content}>
           <button style={styles.button}>Osallistu</button>
-
         </div>
 
       </div>
     </div>
     	<Calendar /> </>
   );
-};
+};*/
 
 const styles = {
   wrapper: {
@@ -33,10 +52,11 @@ const styles = {
     alignContent: 'felex-start',
   },
   card: {
-    background: '#7C5AC2',
+    background: 'rgb(24, 86, 204)',
     borderRadius: '20px',
     padding: '20px',
-    maxWidth: '300px',
+    width: '600px',
+    maxWidth: '50%',
     maxHeight: '300px',
     color: 'white',
     fontFamily: '"Arial", sans-serif',
@@ -45,31 +65,42 @@ const styles = {
     flexDirection: 'column',
     alignItems: 'center',
     margin: 'auto',
+    border: '10px',
+  },
+  timeAndLocation: {
+    color: 'rgba(255, 255, 255, 0.81)',
+    display: 'flex',
+    alignItems: 'right',
+    width: '100%',
+    marginBottom: '15px',
   },
   header: {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     width: '100%',
-    marginBottom: '20px',
   },
   headerText: {
     fontSize: '16px',
   },
+  description: {
+    fontsize: '18px',
+    padding: '20px',
+    marginTop: '20px',
+  },
   profileImage: {
     width: '40px',
-    height: '40px',
+    height: '30px',
     borderRadius: '50%',
     objectFit: 'cover',
   },
   content: {
-    textAlign: 'center',
+    textAlign: 'left',
     width: '100%',
   },
   title: {
     fontSize: '22px',
     fontWeight: 'bold',
-    margin: '0 0 20px 0',
   },
   button: {
     padding: '10px 20px',
@@ -79,9 +110,9 @@ const styles = {
     borderRadius: '20px',
     cursor: 'pointer',
     outline: 'none',
-    width: '80%', // Adjust button width as per design
+    width: '100%', // Adjust button width as per design
     margin: 'auto', // Center button in the card
   },
 };
 
-export default NotificationCard;
+export default NotificationFeed;
