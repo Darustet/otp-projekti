5d6bcf12532abf889751b6b90c71863b322a7931
Darustet <58421506+Darustet@users.noreply.github.com>
2024-03-04 17:42:41 +0200

    Luodaan feediin eventit tietokannasta
    Jokainen tietokantaan tallennettu eventti saa oman postaukset etusivun feediin. Ulkoasu vaatii vielä korjaamista

--- fitness-app/frontend/src/components/NotificationCard/NotificationCard.jsx
+++ fitness-app/frontend/src/components/NotificationCard/NotificationCard.jsx
@@ -1,128 +1,138 @@
-import { useState} from 'react';
+import { useState, useEffect} from 'react';
 import { ellipsis } from 'polished';
 import styled from 'styled-components';
+import { useAuthContext } from "../../context/AuthContext"; 
 
 
-const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut viverra tellus, sit amet sagittis libero. Integer nibh tortor, facilisis vel mollis dapibus, mattis ut nisl. Praesent convallis consequat eros, at interdum lorem lacinia eget. Praesent posuere leo nec tempor pretium. Quisque imperdiet semper ex, in maximus urna porttitor laoreet. Curabitur hendrerit est eget ante pulvinar tristique. Nullam vulputate, nulla vel posuere ullamcorper, mauris leo molestie tellus, a volutpat orci velit eu justo. Curabitur erat lectus, luctus non mauris ut, ultricies ornare diam. Praesent iaculis sapien nec blandit tempus. Praesent vitae gravida nisi. Donec consequat interdum elementum. Donec nec lacus mi. Fusce posuere cursus augue, sit amet vulputate eros dapibus ac. Nulla consequat massa massa, vel hendrerit nunc mattis ut. Morbi lobortis tristique tincidunt. Nulla facilisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut viverra tellus, sit amet sagittis libero. Integer nibh tortor, facilisis vel mollis dapibus, mattis ut nisl. Praesent convallis consequat eros, at interdum lorem lacinia eget. Praesent posuere leo nec tempor pretium. Quisque imperdiet semper ex, in maximus urna porttitor laoreet. Curabitur hendrerit est eget ante pulvinar tristique. Nullam vulputate, nulla vel posuere ullamcorper, mauris leo molestie tellus, a volutpat orci velit eu justo. Curabitur erat lectus, luctus non mauris ut, ultricies ornare diam. Praesent iaculis sapien nec blandit tempus. Praesent vitae gravida nisi. Donec consequat interdum elementum. Donec nec lacus mi. Fusce posuere cursus augue, sit amet vulputate eros dapibus ac. Nulla consequat massa massa, vel hendrerit nunc mattis ut. Morbi lobortis tristique tincidunt. Nulla facilisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut viverra tellus, sit amet sagittis libero. Integer nibh tortor, facilisis vel mollis dapibus, mattis ut nisl. Praesent convallis consequat eros, at interdum lorem lacinia eget. Praesent posuere leo nec tempor pretium. Quisque imperdiet semper ex, in maximus urna porttitor laoreet. Curabitur hendrerit est eget ante pulvinar tristique. Nullam vulputate, nulla vel posuere ullamcorper, mauris leo molestie tellus, a volutpat orci velit eu justo. Curabitur erat lectus, luctus non mauris ut, ultricies ornare diam. Praesent iaculis sapien nec blandit tempus. Praesent vitae gravida nisi. Donec consequat interdum elementum. Donec nec lacus mi. Fusce posuere cursus augue, sit amet vulputate eros dapibus ac. Nulla consequat massa massa, vel hendrerit nunc mattis ut. Morbi lobortis tristique tincidunt. Nulla facilisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut viverra tellus, sit amet sagittis libero. Integer nibh tortor, facilisis vel mollis dapibus, mattis ut nisl. Praesent convallis consequat eros, at interdum lorem lacinia eget. Praesent posuere leo nec tempor pretium. Quisque imperdiet semper ex, in maximus urna porttitor laoreet. Curabitur hendrerit est eget ante pulvinar tristique. Nullam vulputate, nulla vel posuere ullamcorper, mauris leo molestie tellus, a volutpat orci velit eu justo. Curabitur erat lectus, luctus non mauris ut, ultricies ornare diam. Praesent iaculis sapien nec blandit tempus. Praesent vitae gravida nisi. Donec consequat interdum elementum. Donec nec lacus mi. Fusce posuere cursus augue, sit amet vulputate eros dapibus ac. Nulla consequat massa massa, vel hendrerit nunc mattis ut. Morbi lobortis tristique tincidunt. Nulla facilisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut viverra tellus, sit amet sagittis libero. Integer nibh tortor, facilisis vel mollis dapibus, mattis ut nisl. Praesent convallis consequat eros, at interdum lorem lacinia eget. Praesent posuere leo nec tempor pretium. Quisque imperdiet semper ex, in maximus urna porttitor laoreet. Curabitur hendrerit est eget ante pulvinar tristique. Nullam vulputate, nulla vel posuere ullamcorper, mauris leo molestie tellus, a volutpat orci velit eu justo. Curabitur erat lectus, luctus non mauris ut, ultricies ornare diam. Praesent iaculis sapien nec blandit tempus. Praesent vitae gravida nisi. Donec consequat interdum elementum. Donec nec lacus mi. Fusce posuere cursus augue, sit amet vulputate eros dapibus ac. Nulla consequat massa massa, vel hendrerit nunc mattis ut. Morbi lobortis tristique tincidunt. Nulla facilisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut viverra tellus, sit amet sagittis libero. Integer nibh tortor, facilisis vel mollis dapibus, mattis ut nisl. Praesent convallis consequat eros, at interdum lorem lacinia eget. Praesent posuere leo nec tempor pretium. Quisque imperdiet semper ex, in maximus urna porttitor laoreet. Curabitur hendrerit est eget ante pulvinar tristique. Nullam vulputate, nulla vel posuere ullamcorper, mauris leo molestie tellus, a volutpat orci velit eu justo. Curabitur erat lectus, luctus non mauris ut, ultricies ornare diam. Praesent iaculis sapien nec blandit tempus. Praesent vitae gravida nisi. Donec consequat interdum elementum. Donec nec lacus mi. Fusce posuere cursus augue, sit amet vulputate eros dapibus ac. Nulla consequat massa massa, vel hendrerit nunc mattis ut. Morbi lobortis tristique tincidunt. Nulla facilisi`;
 
+// Test
+//const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut viverra tellus, sit amet sagittis libero. Integer nibh tortor, facilisis vel mollis dapibus, mattis ut nisl. Praesent convallis consequat eros, at interdum lorem lacinia eget. Praesent posuere leo nec tempor pretium. Quisque imperdiet semper ex, in maximus urna porttitor laoreet. Curabitur hendrerit est eget ante pulvinar tristique. Nullam vulputate, nulla vel posuere ullamcorper, mauris leo molestie tellus, a volutpat orci velit eu justo. 
 
-const NotificationCard = () => {
+// Gets JSON for one event as an argument
+export const NotificationCard = ({event}) => {
   const [isShowMore, setIsShowMore] = useState(true);
   const toggleReadMore = () => setIsShowMore(show => !show);
-
-
-    return (
-      <div style={styles.wrapper}>
-        <div style={styles.card}>
-          <div style={styles.header}>
-            <span style={styles.title}>Neighbourhood basketball</span>
-            <img style={styles.profileImage} src="path_to_image.jpg" alt="Profile" />
-          </div>
-          <div style={styles.timeAndLocation}>
-            <span style={styles.headerText}>9.3.2024, Lincoln Park Basketball Court</span>
-          </div>
-          <div style={styles.content}>
-            <DescriptionText> {isShowMore ? text.slice(0, 300) : text} </DescriptionText>
-            {text && text.length > 300 && (
-              <ShowMoreText onClick={toggleReadMore}>
-                {isShowMore ? "Show more..." : "Show less"}
-            </ShowMoreText>
-      )}
-          </div>
-          <div styles={styles.content}>
-            <button style={styles.button}>Osallistu</button>
-          </div>
+  const { loginState } = useAuthContext();
   
+
+  // NotificationCard is structured here 
+  // Gets event information from argument
+  return (
+    <div key={event.id}>
+    <div style={styles.wrapper}>
+      <div style={styles.card}>
+        <div style={styles.header}>
+          <span style={styles.title}>{event.title}</span>
+          <img style={styles.profileImage} src="path_to_image.jpg" alt="Profile" />
+        </div>
+        <div style={styles.timeAndLocation}>
+          <span style={styles.headerText}>{event.date}, {event.location}</span>
+        </div>
+        {/* Following implemets "Show more / show less" functionality. 
+        Doesn't show content beyound 300 characters if "Show more" isn't clicked*/}
+        <div style={styles.content}>
+          <DescriptionText> {isShowMore ? event.description.slice(0, 300) : event.description} </DescriptionText>
+          {event.description && event.description.length > 300 && (
+            <ShowMoreText onClick={toggleReadMore}>
+              {isShowMore ? "Show more..." : "Show less"}
+          </ShowMoreText>
+    )}
+        </div>
+        <div styles={styles.content}>
+          <button style={styles.button}>Osallistu</button>
         </div>
       </div>
-    );
-  };
+    </div>
+    </div>
+  );
+};
 
-  const styles = {
-    wrapper: {
-      display: 'flex',
-      height: '100vh', // Full viewport height
-      width: '100%', // Full viewport width
-      flexDirection: 'column',
-      gap: '20px',
-      justifyContent: 'flex-start',
-      alignContent: 'felex-start',
-    },
-    card: {
-      background: 'rgb(24, 86, 204)',
-      borderRadius: '20px',
-      padding: '20px',
-      width: '1000px',
-      maxWidth: '50%',
-      color: 'white',
-      fontFamily: '"Arial", sans-serif',
-      boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      margin: 'auto',
-      border: '10px',
-    },
-    timeAndLocation: {
-      color: 'rgba(255, 255, 255, 0.81)',
-      display: 'flex',
-      alignItems: 'right',
-      width: '100%',
-      marginBottom: '15px',
-    },
-    header: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      width: '100%',
-    },
-    headerText: {
-      fontSize: '16px',
-    },
-    description: {
-      flexGrow: 1,
-      fontsize: '18px',
-      padding: '20px',
-      marginTop: '20px',
-    },
-    profileImage: {
-      width: '40px',
-      height: '30px',
-      borderRadius: '50%',
-      objectFit: 'cover',
-    },
-    content: {
-      textAlign: 'left',
-      width: '100%',
-    },
-    title: {
-      fontSize: '22px',
-      fontWeight: 'bold',
-    },
-    button: {
-      padding: '10px 20px',
-      fontSize: '16px',
-      backgroundColor: 'white',
-      border: 'none',
-      borderRadius: '20px',
-      cursor: 'pointer',
-      outline: 'none',
-      width: '100%', // Adjust button width as per design
-      margin: 'auto', // Center button in the card
-    },
-  };
 
-  const DescriptionText = styled.div`
+// Styling for NotificationCard
+const styles = {
+  wrapper: {
+    display: 'flex',
+    height: '30vh', // Full viewport height
+    width: '100%', // Full viewport width
+    flexDirection: 'column',
+    gap: '20px',
+    justifyContent: 'flex-start',
+    alignContent: 'felex-start',
+  },
+  card: {
+    background: 'rgb(24, 86, 204)',
+    borderRadius: '20px',
+    padding: '20px',
+    width: '500px',
+    maxWidth: '50%',
+    color: 'white',
+    fontFamily: '"Arial", sans-serif',
+    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: 'auto',
+    border: '10px',
+  },
+  timeAndLocation: {
+    color: 'rgba(255, 255, 255, 0.81)',
+    display: 'flex',
+    alignItems: 'right',
+    width: '100%',
+    marginBottom: '15px',
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+  headerText: {
+    fontSize: '16px',
+  },
+  description: {
+    flexGrow: 1,
+    fontsize: '18px',
+    padding: '20px',
+    marginTop: '20px',
+  },
+  profileImage: {
+    width: '40px',
+    height: '30px',
+    borderRadius: '50%',
+    objectFit: 'cover',
+  },
+  content: {
+    textAlign: 'left',
+    width: '100%',
+  },
+  title: {
+    fontSize: '22px',
+    fontWeight: 'bold',
+  },
+  button: {
+    padding: '10px 20px',
+    fontSize: '16px',
+    backgroundColor: 'white',
+    border: 'none',
+    borderRadius: '20px',
+    cursor: 'pointer',
+    outline: 'none',
+    width: '100%', // Adjust button width as per design
+    margin: 'auto', // Center button in the card
+  },
+};
+
+const DescriptionText = styled.div`
   font-size: 14px;
   margin-top: 20px;
   ${({ showMore }) => showMore && ellipsis(undefined, 3)}
 `;
 
-  const ShowMoreText = styled.div`
-    cursor: pointer;
-    font-size: 14px;
-    margin-bottom: 5px;
-    text-decoration: underline;
-  `;
+const ShowMoreText = styled.div`
+  cursor: pointer;
+  font-size: 14px;
+  margin-bottom: 5px;
+  text-decoration: underline;
+`;
 
-export default NotificationCard
--- fitness-app/frontend/src/pages/Feed/Feed.jsx
+++ fitness-app/frontend/src/pages/Feed/Feed.jsx
@@ -1,119 +1,69 @@
+"use client"
 import React from 'react';
 import Calendar from "../../components/Calendar/Calendar.jsx";
-import NotificationCard from "../../components/NotificationCard/NotificationCard";
+import {NotificationCard} from "../../components/NotificationCard/NotificationCard";
+import { useState, useEffect} from 'react';
+
 
 const NotificationFeed = () => {
 
+  const [list, setList] = useState(null);
+  //const [event, setEvent] = useState(null);
+
+
+  // Fetches all events and puts them in a list
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:4000/api/posts/');
+        const eventList = await response.json();
+        setList(eventList);
+        console.log(eventList);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Fetches a single event by id and puts in a const
+  // For testing so currently no functionality to choose id
+  // If used, also uncomment const [event, setEvent]
+  /*
+  useEffect(()=> {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:4000/api/posts/65e366fa566a227827773db8');
+        const eventData = await response.json();
+        setEvent(eventData);
+        {console.log(eventData)}
+      } catch(error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    fetchData();
+    
+  }, []);
+  */
+
+  // First goes through the JSON list of events and
+  // creates NotificationCards for each of them
   return (
     <div>
       <div>
-        {/* Example card, idea to change to a feed consisting of similar cards */}
-        <NotificationCard/>
+        {list && Array.isArray(list) && list.map((event) => (
+          <div>
+          {event && <NotificationCard key={event._id} event={event}/>}
+          </div>
+        ))}
+        {/* For fetch by id testing:
+        {event && <NotificationCard key={event._id} event={event}/>}*/}
         <Calendar/>
       </div>
     </div>
-  )
-}
-
-/*
-const NotificationCard = () => {
-  return (
-    <>
-    <Calendar/>
-    <div style={styles.wrapper}>
-      <div style={styles.card}>
-        <div style={styles.header}>
-          <span style={styles.title}>Neighbourhood basketball</span>
-          <img style={styles.profileImage} src="path_to_image.jpg" alt="Profile" />
-        </div>
-        <div style={styles.timeAndLocation}>
-          <span style={styles.headerText}>Silver · Today @ 9pm</span>
-        </div>
-        <div style={styles.content}>
-          <text tyle={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</text>
-        </div>
-        <div styles={styles.content}>
-          <button style={styles.button}>Osallistu</button>
-        </div>
-
-      </div>
-    </div>
-    	 </>
   );
-};*/
-
-const styles = {
-  wrapper: {
-    display: 'flex',
-    height: '100vh', // Full viewport height
-    width: '100%', // Full viewport width
-    flexDirection: 'column',
-    gap: '20px',
-    justifyContent: 'flex-start',
-    alignContent: 'felex-start',
-  },
-  card: {
-    background: 'rgb(24, 86, 204)',
-    borderRadius: '20px',
-    padding: '20px',
-    width: '600px',
-    maxWidth: '50%',
-    maxHeight: '300px',
-    color: 'white',
-    fontFamily: '"Arial", sans-serif',
-    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    margin: 'auto',
-    border: '10px',
-  },
-  timeAndLocation: {
-    color: 'rgba(255, 255, 255, 0.81)',
-    display: 'flex',
-    alignItems: 'right',
-    width: '100%',
-    marginBottom: '15px',
-  },
-  header: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
-  },
-  headerText: {
-    fontSize: '16px',
-  },
-  description: {
-    fontsize: '18px',
-    padding: '20px',
-    marginTop: '20px',
-  },
-  profileImage: {
-    width: '40px',
-    height: '30px',
-    borderRadius: '50%',
-    objectFit: 'cover',
-  },
-  content: {
-    textAlign: 'left',
-    width: '100%',
-  },
-  title: {
-    fontSize: '22px',
-    fontWeight: 'bold',
-  },
-  button: {
-    padding: '10px 20px',
-    fontSize: '16px',
-    backgroundColor: 'white',
-    border: 'none',
-    borderRadius: '20px',
-    cursor: 'pointer',
-    outline: 'none',
-    width: '100%', // Adjust button width as per design
-    margin: 'auto', // Center button in the card
-  },
 };
 
 export default NotificationFeed;
