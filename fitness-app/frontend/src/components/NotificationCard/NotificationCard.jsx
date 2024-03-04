06edf1b33f14edfedf5a7beb7c7357629034f7ee
Darustet <58421506+Darustet@users.noreply.github.com>
2024-03-04 10:28:42 +0200

    NotificationCard esimerkkiin lisätty päivämäärä/paikka luonnnos

--- fitness-app/frontend/src/components/NotificationCard/NotificationCard.jsx
+++ fitness-app/frontend/src/components/NotificationCard/NotificationCard.jsx
@@ -9,7 +9,7 @@ const NotificationCard = () => {
             <img style={styles.profileImage} src="path_to_image.jpg" alt="Profile" />
           </div>
           <div style={styles.timeAndLocation}>
-            <span style={styles.headerText}>Silver · Today @ 9pm</span>
+            <span style={styles.headerText}>9.3.2024, Lincoln Park Basketball court</span>
           </div>
           <div style={styles.content}>
             <text tyle={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</text>
