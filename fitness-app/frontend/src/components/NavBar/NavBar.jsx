7c010de2554f8c31504c06e2aea090965c5e401f
PatrickJaloScott <patrickjalo.scott@gmail.com>
2024-03-06 15:35:21 +0200

    Title: creating feature for light/dark theme * NavBar.jsx: adding lines to enable changing page theme between light/dark

--- fitness-app/frontend/src/components/NavBar/NavBar.jsx
+++ fitness-app/frontend/src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, createContext} from 'react';
 import style from "./NavBar.module.scss";
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../../context/AuthContext";
@@ -11,13 +11,13 @@ export default function NavBar() {
 	//const filteredData = linkData.filter(item => item.name);
 
 	const { loginState } = useAuthContext();
+	const [pageTheme, setPageTheme] = useState(false);
 	console.log(loginState.id);
 	return (
 		<nav className={style["Nav-bar"]}>
 			<ProfileIcon />
 			<HomeIcon />
 			<LogoutIcon  color= {"red"}/>
-
 			{loginState.id ? <>
 				<Link to="/">Feed</Link>
 				<Link to="/profile">Profile</Link>
@@ -27,6 +27,10 @@ export default function NavBar() {
 				<Link to="/register">Register</Link>
 			</>
 			}
+			<div>
+				<input type="checkbox" onClick={()=>setPageTheme(!pageTheme)}/>
+				{pageTheme ? "Light" : "Dark"}
+			</div>
 		</nav>
 	);
 }
