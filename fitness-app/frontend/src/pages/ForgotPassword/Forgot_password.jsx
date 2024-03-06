a2a7cce35282e470a12c25437e7c943481f199c4
Sami <Sami.AboRamadan@metropolia.fi>
2024-03-06 10:33:11 +0200

    updated

--- fitness-app/frontend/src/pages/ForgotPassword/Forgot_password.jsx
+++ fitness-app/frontend/src/pages/ForgotPassword/Forgot_password.jsx
@@ -1,38 +1,39 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./ForgotPassword.module.scss";
+import Offcanvas from "fitness-app/frontend/src/pages/ForgotPassword/OffCanvas.jsx"
 
 const ForgotPassword = () => {
-return (
-<>
-<meta charSet="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>Forgot Password</title>
-<link rel="stylesheet" href={styles.container} />
+    return (
+        <>
+            <Offcanvas /> {/* Add the OffcanvasExample component */}
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Forgot Password</title>
+            <link rel="stylesheet" href={styles.container} />
 
-<div className={styles.container}>
-    <div className={styles.formContainer}>
-        <h2>Forgot Password</h2>
-        <div className={styles.formGroup}>
-            <label htmlFor="email">Email:</label>
-            <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Enter your email address"
-            />
-        </div>
-        <div className={styles.formGroup}>
-            <button type="button">Reset Password</button>
-        </div>
-        <div className={styles.options}>
-            <Link to="/login">Back to Login</Link>
-        </div>
-    </div>
-</div>
-</>
-);
+            <div className={styles.container}>
+                <div className={styles.formContainer}>
+                    <h2>Forgot Password</h2>
+                    <div className={styles.formGroup}>
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Enter your email address"
+                        />
+                    </div>
+                    <div className={styles.formGroup}>
+                        <button type="button">Reset Password</button>
+                    </div>
+                    <div className={styles.options}>
+                        <Link to="/login">Back to Login</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default ForgotPassword;
